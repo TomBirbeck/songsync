b8d3d6d06bb9d53214ec77090a7f3233a2034402
unknown <tombirbeck47@gmail.com>
2023-05-29 23:36:37 +0100

    added in icons for correct/incorrect and styled

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return(
-        <h1 className="text-3xl text-white">Song Sync</h1>
+        <h1 className="text-3xl md:text-6xl md:my-4 text-white">Song Sync</h1>
     )
 }
 
--- src/components/PreviousGuesses/index.tsx
+++ src/components/PreviousGuesses/index.tsx
@@ -1,22 +1,23 @@
 import { GuessIProps } from "../types"
+import {FaCheck, FaTimes} from 'react-icons/fa'
 
 const PreviousGuesses = ({guesses, answer}: GuessIProps) => {
     return (
         <div className="bg-white w-10/12 md:w-1/2 py-1 rounded-lg text-lg">
-        <div className="flex justify-around">
-            <p>{guesses.guess1}</p>{guesses.guess1 ==='Guess 1' ? <span></span> : guesses.guess1 === answer? <span>Y</span> : <span>X</span>}
+        <div className="flex">
+            <p className="w-1/2 text-center">{guesses.guess1}</p>{guesses.guess1 ==='Guess 1' ? <span></span> : guesses.guess1.toLowerCase() === answer.toLowerCase() ? <span className="w-1/2 text-xl flex items-center justify-center text-green-600"><FaCheck/></span> : <span className="w-1/2 text-xl flex items-center justify-center text-red-600"><FaTimes/></span>}
         </div>
-        <div className="flex justify-around">
-            <p>{guesses.guess2}</p>{guesses.guess2 ==='Guess 2' ? <span></span> : guesses.guess2 === answer? <span>Y</span> : <span>X</span>}
+        <div className="flex">
+            <p className="w-1/2 text-center">{guesses.guess2}</p>{guesses.guess2 ==='Guess 2' ? <span></span> : guesses.guess2.toLowerCase() === answer.toLowerCase() ? <span className="w-1/2 text-xl flex items-center justify-center text-green-600"><FaCheck/></span> : <span className="w-1/2 text-xl flex items-center justify-center text-red-600"><FaTimes/></span>}
         </div>
-        <div className="flex justify-around">
-            <p>{guesses.guess3}</p>{guesses.guess3 ==='Guess 3' ? <span></span> : guesses.guess3 === answer? <span>Y</span> : <span>X</span>}
+        <div className="flex">
+            <p className="w-1/2 text-center">{guesses.guess3}</p>{guesses.guess3 ==='Guess 3' ? <span></span> : guesses.guess3.toLowerCase() === answer.toLowerCase() ? <span className="w-1/2 text-xl flex items-center justify-center text-green-600"><FaCheck/></span> : <span className="w-1/2 text-xl flex items-center justify-center text-red-600"><FaTimes/></span>}
         </div>
-        <div className="flex justify-around">
-            <p>{guesses.guess4}</p>{guesses.guess4 ==='Guess 4' ? <span></span> : guesses.guess4 === answer? <span>Y</span> : <span>X</span>}
+        <div className="flex">
+            <p className="w-1/2 text-center">{guesses.guess4}</p>{guesses.guess4 ==='Guess 4' ? <span></span> : guesses.guess4.toLowerCase() === answer.toLowerCase() ? <span className="w-1/2 text-xl flex items-center justify-center text-green-600"><FaCheck/></span> : <span className="w-1/2 text-xl flex items-center justify-center text-red-600"><FaTimes/></span>}
         </div>
-        <div className="flex justify-around">
-            <p>{guesses.guess5}</p>{guesses.guess5 ==='Guess 5' ? <span></span> : guesses.guess5 === answer? <span>Y</span> : <span>X</span>}
+        <div className="flex">
+            <p className="w-1/2 text-center">{guesses.guess5}</p>{guesses.guess5 ==='Guess 5' ? <span></span> : guesses.guess5.toLowerCase() === answer.toLowerCase() ? <span className="w-1/2 text-xl flex items-center justify-center text-green-600"><FaCheck/></span> : <span className="w-1/2 text-xl flex items-center justify-center text-red-600"><FaTimes/></span>}
         </div>
         </div>
     )
