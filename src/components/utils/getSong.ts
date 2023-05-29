24f69ac5f1f088f006312aec9b0685a09623f00e
unknown <tombirbeck47@gmail.com>
2023-05-29 23:13:02 +0100

    added styling to result component, tweaked some of the font sizes

--- src/components/utils/getSong.ts
+++ src/components/utils/getSong.ts
@@ -1,10 +1,4 @@
 const getSong = async () => {
-    const token: string = (import.meta.env.VITE_TOKEN as string);
-    const user_id: string = (import.meta.env.USER_ID as string);
-    const res = await fetch(`https://www.stands4.com/services/v2/lyrics.php?uid=${user_id}&tokenid=${token}&term=forever%20young&artist=Alphaville&format=json`);
-    const data = await res.json();
-    console.log(data)
-    return data
 }
 
 export default getSong
