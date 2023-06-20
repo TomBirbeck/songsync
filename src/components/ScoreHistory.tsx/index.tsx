b0b96cf89b05f70a18e20a0cafbce94c160e1ce7
unknown <tombirbeck47@gmail.com>
2023-06-20 21:54:35 +0100

    finished instructions, tweaked style and size of pop outs

--- src/components/ScoreHistory.tsx/index.tsx
+++ src/components/ScoreHistory.tsx/index.tsx
@@ -6,12 +6,8 @@ type ScoreHistoryIprops = {
 }
 
 const ScoreHistory = ({attempts, completions, currentStreak, bestStreak} : ScoreHistoryIprops) => {
-    //player makes guess or skips
-    //local storage will be updated about attempts but only once per day
-    //completions
-    // update the current streak or best streak if answer changes them
     return (
-        <div className="flex flex-col justify-center items-center gap-4 h-3/4 w-3/4 md:w-1/2 p-4 md:absolute md:translate-y-1/4 bg-purple-400 z-10 rounded-lg text-xl md:text-3xl">
+        <div className="flex flex-col justify-center items-center gap-4 lg:gap-8 w-full lg:h-3/5 lg:w-1/4 p-4 md:absolute md:translate-y-1/4 bg-purple-400 opacity-1 z-10 rounded-lg text-xl lg:text-3xl">
             <h3 className="underline text-4xl">Your Stats</h3>
             <div className="flex flex-col gap-4">
             <p>Attempted Songs: <span>{attempts}</span></p>
