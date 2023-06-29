3b96db9ed6b0b5faf5baca7ec516df85b6f1c667
unknown <tombirbeck47@gmail.com>
2023-06-29 23:39:51 +0100

    created countdown to next day component, added logic to count down

--- src/components/ScoreHistory.tsx/index.tsx
+++ src/components/ScoreHistory.tsx/index.tsx
@@ -1,3 +1,5 @@
+import CountDownTimer from "../CountDownTimer";
+
 type ScoreHistoryIprops = {
     attempts: number;
     completions: number;
@@ -22,20 +24,22 @@ const ScoreHistory = ({attempts, completions, currentStreak, bestStreak, setScor
                 <p>Current streak: <span>{currentStreak}</span></p>
                 <p>Best Streak: <span>{bestStreak}</span></p>
                 </div>
+            <CountDownTimer/>
             </div>
             </div>
         );
                     } else {
                         return (
                             <div className="flex flex-col justify-center items-center gap-4 lg:gap-8 w-full lg:h-3/5 md:w-3/5 lg:w-1/4 p-4 bg-purple-400 opacity-1 z-30 rounded-lg text-xl lg:text-2xl border-4 border-solid border-white">
-                            <h3 className="underline text-4xl">Your Stats</h3>
-                            <div className="flex flex-col gap-4">
-                            <p>Attempted Songs: <span>{attempts}</span></p>
-                            <p>Correct Guesses: <span>{completions}</span></p>
-                            <p>Current streak: <span>{currentStreak}</span></p>
-                            <p>Best Streak: <span>{bestStreak}</span></p>
+                                <h3 className="underline text-4xl">Your Stats</h3>
+                                <div className="flex flex-col gap-4">
+                                <p>Attempted Songs: <span>{attempts}</span></p>
+                                <p>Correct Guesses: <span>{completions}</span></p>
+                                <p>Current streak: <span>{currentStreak}</span></p>
+                                <p>Best Streak: <span>{bestStreak}</span></p>
+                                </div>
+                                <CountDownTimer/>
                             </div>
-                        </div>
                         )
                     }
 
