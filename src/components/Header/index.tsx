135b707dcf80622ce821131af887f179b2ad2bfa
unknown <tombirbeck47@gmail.com>
2023-07-09 09:46:53 +0100

    added new font, adjusted font weights, added pulse animation to underline of header and adjusted result section width for tablets

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ const Header = () => {
     return(
         <div className="md:mb-4">
             <h1 className="w-full text-center text-3xl md:text-5xl text-white">Songsmith</h1>
-        <div className="w-full h-4 bg-purple-400"></div>
+        <div className="w-full h-4 bg-purple-400 opacity-80 animate-pulse"></div>
     </div>
     )
 }
